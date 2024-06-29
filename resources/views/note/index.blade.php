9d01a9a3455f70d509adf907c9570e0c90fa63d0
azzy <78948500+lAzlinz@users.noreply.github.com>
2024-06-30 00:16:51 +0800

    create view for list of notes
    We created the homepage for our notes using foreach to create note items.

--- resources/views/note/index.blade.php
+++ resources/views/note/index.blade.php
@@ -1,3 +1,20 @@
 <x-layout-example>
     <h1>Welcome to your notes</h1>
+    <div class="note-container">
+        <a href={{ route('note.create') }} class="new-note-btn">New Note</a>
+        <div class="notes">
+            @foreach ($notes as $note)
+                <div class="note">
+                    <div class="note-body">
+                        {{ Str::words($note->note, 30) }}
+                    </div>
+                    <div class="note-buttons">
+                        <a href={{ route('note.show', $note) }} class="note-show-button">View</a>
+                        <a href={{ route('note.edit', $note) }} class="note-edit-button">Edit</a>
+                        <button class="note-delete-button">Delete</button>
+                    </div>
+                </div>
+            @endforeach
+        </div>
+    </div>
 </x-layout-example>
