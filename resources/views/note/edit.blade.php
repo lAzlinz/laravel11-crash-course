922180ae1f061c4a7335b7ccb489fc0931c269dc
azzy <78948500+lAzlinz@users.noreply.github.com>
2024-06-30 01:01:08 +0800

    added csrf
    added the @csrf tag. This prevents other websites from making a post request to your website. Also added the @method() tag so in edit we can override the POST method to be PUT since PUT is needed by the update() method in the NoteController

--- resources/views/note/edit.blade.php
+++ resources/views/note/edit.blade.php
@@ -2,6 +2,8 @@
     <div class="note-container single-note">
         <h1>Edit note: {{ $note->created_at }}</h1>
         <form action={{ route('note.update', $note) }} method="POST" class="note">
+            @csrf
+            @method('PUT')
             <textarea name="note" rows="10" class="note-body" placeholder="Enter your note here...">{{ $note->note }}</textarea>
             <div class="note-buttons">
                 <a href={{ URL::previous() }} class="note-cancel-button">Cancel</a>
