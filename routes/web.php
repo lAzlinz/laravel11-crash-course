73440ebf5f86943a9b4258a4ec558555b967804e
azzy <78948500+lAzlinz@users.noreply.github.com>
2024-06-29 23:23:37 +0800

    routing note [one line]
    This line of code generates the same code that was removed.

--- routes/web.php
+++ routes/web.php
@@ -5,10 +5,4 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', [WelcomeController::class, 'welcome'])->name('welcome');
 
-Route::get('/note', [NoteController::class, 'index'])->name('note.index');
-Route::get('note/create', [NoteController::class, 'create'])->name('note.create');
-Route::post('/note', [NoteController::class, 'store'])->name('note.store');
-Route::get('/note/{id}', [NoteController::class, 'show'])->name('note.show');
-Route::get('note/{id}/edit', [NoteController::class, 'edit'])->name('note.edit');
-Route::put('note/{id}', [NoteController::class, 'update'])->name('note.update');
-Route::delete('note/{id}', [NoteController::class, 'delete'])->name('note.,delete');
+Route::resource('note', NoteController::class);
