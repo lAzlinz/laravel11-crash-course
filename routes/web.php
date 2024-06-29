329931e289d6c7a90a13c37d0e9ed00a43ef47eb
azzy <78948500+lAzlinz@users.noreply.github.com>
2024-06-30 01:46:06 +0800

    move notes route
    move the routes for the notes so that they aren't accessible if the user is not authenticated nor are they verified.

--- routes/web.php
+++ routes/web.php
@@ -3,15 +3,9 @@
 use App\Http\Controllers\{ProfileController, NoteController};
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
+Route::middleware(['auth', 'verified'])->group(function () {
+    Route::resource('note', NoteController::class);
 });
-
-Route::resource('note', NoteController::class);
-
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
